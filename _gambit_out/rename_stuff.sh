61a454fa3b87141b11c5273826c3c125664708c3
DanieLion55 <daniel@certora.com>
2023-09-28 14:25:29 +0300

    attempt_rename_2
    Couldn't run CertoraMutate with my mutations, so just gonna use some bash scripts

---
+++ _gambit_out/rename_stuff.sh
@@ -0,0 +1,23 @@
+
+
+mkdir ./_gambit_out/mutants_flattened
+cp ./_gambit_out/mutants/1/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m1.sol
+cp ./_gambit_out/mutants/2/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m2.sol
+cp ./_gambit_out/mutants/3/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m3.sol
+cp ./_gambit_out/mutants/4/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m4.sol
+cp ./_gambit_out/mutants/5/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m5.sol
+cp ./_gambit_out/mutants/6/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m6.sol
+cp ./_gambit_out/mutants/7/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m7.sol
+cp ./_gambit_out/mutants/8/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m8.sol
+cp ./_gambit_out/mutants/9/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m9.sol
+cp ./_gambit_out/mutants/10/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m10.sol
+cp ./_gambit_out/mutants/11/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m11.sol
+cp ./_gambit_out/mutants/12/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m12.sol
+cp ./_gambit_out/mutants/13/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m13.sol
+cp ./_gambit_out/mutants/14/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m14.sol
+cp ./_gambit_out/mutants/15/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m15.sol
+cp ./_gambit_out/mutants/16/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m16.sol
+cp ./_gambit_out/mutants/17/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m17.sol
+cp ./_gambit_out/mutants/18/contracts/token/MiniMeToken.sol ./_gambit_out/mutants_flattened/MiniMeToken.m18.sol
+
+
